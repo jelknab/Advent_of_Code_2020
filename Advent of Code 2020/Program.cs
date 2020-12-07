2620ea52fca1fd6ebc712d00e8dcab6597cd1809
Jelle_M <jelle.metzlar@outlook.com>
2020-12-07 18:56:37 +0100

    Added reflection for getting today's object so I don't have to edit the main function.

--- Advent of Code 2020/Program.cs
+++ Advent of Code 2020/Program.cs
@@ -6,18 +6,27 @@ namespace Advent_of_Code_2020
     {
         static void Main(string[] args)
         {
-            IDay day = new Day7.Day7();
+            var dayType = Type.GetType($"Advent_of_Code_2020.Day{DateTime.Today.Day}.Day{DateTime.Today.Day}")
+                    ?? throw new Exception("Please stick to naming scheme format."); 
+            
+            var day = (IDay) Activator.CreateInstance(dayType)
+                      ?? throw new Exception("Could not make instance of today's challenge.");
+
             try
             {
+                Console.WriteLine("Solution problem 1:");
                 day.SolveProblem1();
             }
             catch (NotImplementedException e)
             {
                 Console.WriteLine("Day 1 not solved yet");
             }
+
+            Console.WriteLine();
             
             try
             {
+                Console.WriteLine("Solution problem 2:");
                 day.SolveProblem2();
             }
             catch (NotImplementedException e)
