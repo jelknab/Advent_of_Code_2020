dcf740eb0e751743ad5c0c8c62fb39923b974cb0
Jelle Metzlar <jelle.metzlar@outlook.com>
2020-12-09 13:29:40 +0100

    Solved day 9

---
+++ Advent of Code 2020/Day9/Day9.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Advent_of_Code_2020.Day9
+{
+    public class Day9 : IDay
+    {
+        public static long[] XMASParser(string file)
+        {
+            return new ResourceReader<long>(file)
+                .LineReader(long.Parse)
+                .ToArray();
+        }
+
+        private static (int a, int b) FindPairThatSumsInput(long[] input, int preample, int index)
+        {
+            for (var y = index - preample; y < index; y++)
+                for (var x = y + 1; x < index; x++)
+                    if (input[y] + input[x] == input[index]) return (y, x);
+
+            throw new InvalidDataException("No pair that sums could be made");
+        }
+
+        public static long FindWrongNumber(long[] input, int preample)
+        {
+            for (var i = preample; i < input.Length; i++)
+            {
+                try
+                {
+                    FindPairThatSumsInput(input, preample, i);
+                }
+                catch (InvalidDataException e)
+                {
+                    return input[i];
+                }
+            }
+            
+            throw new Exception("All values seem correct.");
+        }
+
+        public static List<long> FindSetOfNumbersThatSumInput(long[] numbers, long input)
+        {
+            var inputIndex = Array.IndexOf(numbers, input);
+
+            for (var i = 0; i < inputIndex; i++)
+            {
+                var sum = numbers[i];
+
+                for (var j = i + 1; j < inputIndex; j++)
+                {
+                    sum += numbers[j];
+
+                    if (sum > input) break;
+
+                    if (sum == input)
+                    {
+                        return numbers.Where((val, index) => index >= i && index <= j).ToList();
+                    }
+                }
+            }
+
+            throw new Exception("not found");
+        } 
+        
+        public void SolveProblem1()
+        {
+            var input = XMASParser("Advent_of_Code_2020.Day9.input.txt");
+
+            Console.WriteLine(FindWrongNumber(input, 25));
+        }
+
+        public void SolveProblem2()
+        {
+            var input = XMASParser("Advent_of_Code_2020.Day9.input.txt");
+
+            var sumNumbers = FindSetOfNumbersThatSumInput(input, FindWrongNumber(input, 25));
+            Console.WriteLine(sumNumbers.Min() + sumNumbers.Max());
+        }
+    }
+}
---
+++ Tests/Day9Tests.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Advent_of_Code_2020.Day9;
+using Xunit;
+
+namespace Tests
+{
+    public class Day9Tests
+    {
+        [Fact]
+        public void TestWrongNumberFinder()
+        {
+            var input = Day9.XMASParser("Advent_of_Code_2020.Day9.SampleInput.txt");
+            
+            Assert.Equal(127, Day9.FindWrongNumber(input, 5));
+        }
+
+        [Fact]
+        public void TestFindSetOfNumbersThatSum()
+        {
+            var input = Day9.XMASParser("Advent_of_Code_2020.Day9.SampleInput.txt");
+            var answer = Day9.FindSetOfNumbersThatSumInput(input, 127);
+            
+            Assert.Equal(15, answer.Min());
+            Assert.Equal(47, answer.Max());
+        }
+    }
+}
